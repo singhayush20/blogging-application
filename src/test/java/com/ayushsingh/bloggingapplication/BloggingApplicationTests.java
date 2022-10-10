68c02a96f76888eda837fcebb67a512b69a3778d
singhayush20 <ayushsingh20november@gmail.com>
2022-10-10 22:38:06 +0530

    first commit

---
+++ src/test/java/com/ayushsingh/bloggingapplication/BloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushsingh.bloggingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
