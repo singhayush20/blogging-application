cc7d7379e17da32fb8ca4003a9cb504f71fc0189
singhayush20 <ayushsingh20november@gmail.com>
2022-10-11 14:47:53 +0530

    Test for @Autowired on UserRep written

--- src/test/java/com/ayushsingh/bloggingapplication/BloggingApplicationTests.java
+++ src/test/java/com/ayushsingh/bloggingapplication/BloggingApplicationTests.java
@@ -1,13 +1,30 @@
 package com.ayushsingh.bloggingapplication;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ayushsingh.bloggingapplication.repositories.UserRep;
 
 @SpringBootTest
 class BloggingApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+
+	//Add this code in the BloggingApplicationTests.java
+	//to check for the autowiring of UserRep
+	@Autowired
+	private UserRep userRepo;
+	@Test
+	public void repoTest(){
+		String className=this.userRepo.getClass().getName();
+		String packageName=this.userRepo.getClass().getPackageName();
+		System.out.println("UserRep: className: "+className);
+		System.out.println("UserRep: packageName: "+packageName);
 	}
 
 }
