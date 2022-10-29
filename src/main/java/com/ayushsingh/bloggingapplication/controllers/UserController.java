203335c94f8c57d1c2f68702031b0eb51d18fa40
singhayush20 <ayushsingh20november@gmail.com>
2022-10-29 16:08:54 +0530

    Minor Commit

--- src/main/java/com/ayushsingh/bloggingapplication/controllers/UserController.java
+++ src/main/java/com/ayushsingh/bloggingapplication/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.ayushsingh.bloggingapplication.controllers;
 
-
 import java.util.List;
 
 import javax.validation.Valid;
@@ -25,55 +24,61 @@ import com.ayushsingh.bloggingapplication.services.UserService;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    
+
     @Autowired
     private UserService userService;
 
-    //POST- create user
+    // POST- create user
     @PostMapping("/")
-    public ResponseEntity<UserDto> createUser(@Valid /*Enable validation*/ @RequestBody UserDto userDto){
-        UserDto createdUserDto=this.userService.createUser(userDto);
-        return new ResponseEntity<UserDto>(createdUserDto,HttpStatus.CREATED);
+    public ResponseEntity<UserDto> createUser(@Valid /* Enable validation */ @RequestBody UserDto userDto) {
+        UserDto createdUserDto = this.userService.createUser(userDto);
+        return new ResponseEntity<UserDto>(createdUserDto, HttpStatus.CREATED);
     }
-    //PUT- update user
-    @PutMapping("/{userid}") //userid is a path uri variable, we will use it fetch data
-    //@PathVariable("userid") if the variable name is same as mapping, we need not specify like this
-    public ResponseEntity<UserDto> updateUser(/*Enable Validation*/ @Valid @RequestBody UserDto userDto,@PathVariable("userid") Integer uid){
-        UserDto updatedUser=this.userService.updateUser(userDto, uid);
+
+    // PUT- update user
+    @PutMapping("/{userid}") // userid is a path uri variable, we will use it fetch data
+    // @PathVariable("userid") if the variable name is same as mapping, we need not
+    // specify like this
+    public ResponseEntity<UserDto> updateUser(/* Enable Validation */ @Valid @RequestBody UserDto userDto,
+            @PathVariable("userid") Integer uid) {
+        UserDto updatedUser = this.userService.updateUser(userDto, uid);
         return ResponseEntity.ok(updatedUser);
 
     }
-    //DELETE- delete user
+    // DELETE- delete user
     // @DeleteMapping("/{userid}")
-    //     public ResponseEntity<?> deleteUser(@PathVariable("userId") Integer uid){
-    //         // ResponseEntity<?> deletedUser=
-    //         this.userService.deleteUser(uid);
-    //         return  ResponseEntity.ok(Map.of("message","user deleted successfully"));
-    //         // OR
-    //          return ResponseEntity(Map.of("message","user deleted successfully"),HttpStatus.OK);
+    // public ResponseEntity<?> deleteUser(@PathVariable("userId") Integer uid){
+    // // ResponseEntity<?> deletedUser=
+    // this.userService.deleteUser(uid);
+    // return ResponseEntity.ok(Map.of("message","user deleted successfully"));
+    // // OR
+    // return ResponseEntity(Map.of("message","user deleted
+    // successfully"),HttpStatus.OK);
 
-    //     }
-        //OR
-       
-            //ADMIN ROLE CAN USE THIS API
-        @DeleteMapping("/{userid}")
-        @PreAuthorize("hasRole('ROLE_ADMIN')") //NOW ONLY THE USERS WITH ADMIN ROLES WILL BE PERMITTED
-        public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userid") Integer uid){
-            
-            // ResponseEntity<?> deletedUser=
-            this.userService.deleteUser(uid);
-            return new ResponseEntity<ApiResponse>(new ApiResponse("user deleted successfully",true),HttpStatus.OK);
+    // }
+    // OR
 
-        }
-    //GET- user get
+    // ADMIN ROLE CAN USE THIS API
+    @PreAuthorize("hasRole('ROLE_ADMIN')") // NOW ONLY THE USERS WITH ADMIN ROLES WILL BE PERMITTED
+    @DeleteMapping("/{userid}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userid") Integer uid) {
+
+        // ResponseEntity<?> deletedUser=
+        this.userService.deleteUser(uid);
+        return new ResponseEntity<ApiResponse>(new ApiResponse("user deleted successfully", true), HttpStatus.OK);
+
+    }
+
+    // GET- user get
     @GetMapping("/")
-    public ResponseEntity<List<UserDto>> getAllusers(){
+    public ResponseEntity<List<UserDto>> getAllusers() {
         return ResponseEntity.ok(this.userService.getAllUsers());
     }
+
     @GetMapping("/{userid}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable("userid") Integer uid){
-       
+    public ResponseEntity<UserDto> getUserById(@PathVariable("userid") Integer uid) {
+
         return ResponseEntity.ok(this.userService.getUserById(uid));
     }
-    
+
 }
