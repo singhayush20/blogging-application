eeda38641f8014911f780cd1780910a460dd0c8f
singhayush20 <ayushsingh20november@gmail.com>
2023-11-22 02:37:59 +0530

    cred

--- src/main/java/com/ayushsingh/bloggingapplication/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/ayushsingh/bloggingapplication/security/JwtAuthenticationEntryPoint.java
@@ -22,6 +22,8 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
+
+
                 resolver.resolveException(request, response, null, authException);
     }
     
